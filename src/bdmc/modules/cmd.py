852c128f6e6acabbd61827c21c7e2a5dfdd16d61
Whth <88489697+Whth@users.noreply.github.com>
2024-06-02 17:50:47 +0800

    fix(cmd): bad end char

--- src/bdmc/modules/cmd.py
+++ src/bdmc/modules/cmd.py
@@ -6,12 +6,12 @@ class CMD(Enum):
     CMDs that is a constant
     """
 
-    RESET = b"RESET\n"  # 重新启动
-    FULL_STOP = b"v0\n"  # 停止电机
+    RESET = b"RESET\r"  # 重新启动
+    FULL_STOP = b"v0\r"  # 停止电机
 
-    ADL = b"ADL\n"  # 定义逆时针方向为正
-    ADR = b"ADR\n"  # 定义顺时针方向为正
+    ADL = b"ADL\r"  # 定义逆时针方向为正
+    ADR = b"ADR\r"  # 定义顺时针方向为正
 
-    NPOFF = b"NPOFF\n"  # 关闭位置应答
-    NVOFF = b"NVOFF\n"  # 关闭速度应答
-    EEPSAVE = b"EEPSAVE\n"  # 将参数写入驱动器EERPROM
+    NPOFF = b"NPOFF\r"  # 关闭位置应答
+    NVOFF = b"NVOFF\r"  # 关闭速度应答
+    EEPSAVE = b"EEPSAVE\r"  # 将参数写入驱动器EERPROM
